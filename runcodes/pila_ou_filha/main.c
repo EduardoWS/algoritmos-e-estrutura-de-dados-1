d7e30b06d77ffa192019e627bf5c857c1da1d32b
Eduardo Ribeiro <eduardo.ribeiro@wr.sc.usp.br>
2023-10-06 14:21:41 -0300

    runcodes

--- runcodes/pila_ou_filha/main.c
+++ runcodes/pila_ou_filha/main.c
@@ -7,17 +7,22 @@ int main(){
     int N;
     scanf("%d", &N);
 
-    if(N > 100 || N < 1) return 1;
+    if(N > 100 || N < 1) return 1;  // Verifica se o número de casos de teste está dentro dos limites
 
     t_fila F;
     inicializar(&F);
 
     int aux;
     for(int i=0; i<N; i++){
+        // Chama a função para cada caso de teste e armazena o resultado em 'aux'
         aux = pilaOuFilha();
+
+        // Enfileira o resultado na fila
         enfileirar(aux, &F);
     }
 
+
+    // Imprime os resultados
     while(!vazia(&F)){
         aux = desenfileirar(&F);
         if(aux == 1){
--- runcodes/pila_ou_filha/pila_ou_filha.c
+++ runcodes/pila_ou_filha/pila_ou_filha.c
@@ -12,41 +12,51 @@ int pilaOuFilha(){
     int I;
     int removido;
     int flag_pilha = 1, flag_fila = 1;
+    int cont_r=0;
 
     t_pilha P;
     t_fila F;
     inicializarPilha(&P);
     inicializar(&F);
 
-    int eh_Pilha, eh_Fila; // 0 = não; 1 = sim
+    int eh_Pilha=0, eh_Fila=0; // 0 = não; 1 = sim
 
     for(int i=0; i<K; i++){
         scanf(" %c", &C);
         scanf("%d", &I);
 
-        if(i==0 && C == 'r') return IMPOSSIVEL;
+        //if(i==0 && C == 'r') return IMPOSSIVEL;
+        //if(C == 'r' && (vaziaPilha(&P) || vazia(&F))) return IMPOSSIVEL; 
 
         if(C == 'i') {
-            
+            // Insere na pilha e na fila
             push(I, &P);
             enfileirar(I, &F);
 
         }else if(C == 'r'){
+            cont_r++;
+            if(vaziaPilha(&P) || vazia(&F)) return IMPOSSIVEL;
+
             if(flag_pilha){
                 removido = pop(&P);
                 eh_Pilha = (removido == I)? 1 : 0;
-                if(eh_Pilha == 0) flag_pilha = 0;
+                if(eh_Pilha == 0) flag_pilha = 0; // Se não corresponde, pilha é impossível
             }
 
             if(flag_fila){
                 eh_Fila = (I == F.fila[F.inicio])? 1 : 0;
                 desenfileirar(&F);
-                if(eh_Fila == 0) flag_fila = 0;
+                if(eh_Fila == 0) flag_fila = 0;  // Se não corresponde, fila é impossível
             }
             
 
         }
 
+    }
+
+    if(cont_r == 0){
+        eh_Pilha = 1;
+        eh_Fila = 1;
     }
 
     if(eh_Pilha == 1 && eh_Fila == 0) return PILHA;
